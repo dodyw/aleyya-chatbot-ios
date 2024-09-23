466fddb40b24ee7d8decc498e30a63211d4ea996
dody <do@nicecoder.com>
2024-09-23 09:38:34 +0700

    Initial Commit

---
+++ Aleyya/AleyyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AleyyaApp.swift
+//  Aleyya
+//
+//  Created by dody on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AleyyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
